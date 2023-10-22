230f25c4944c8d90c4136bfff6aa856f0573adcb
Marco Kleimaier <marco2.kleimaier@gmx.de>
2023-10-22 18:34:24 +0200

    solve single cube func and applied it

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from inference import *
 import os
 
 
+load_path = os.path.join(".", "models", "model-5.h5")
+
+# train the model
 """train(
     n_epochs=1,
     n_iterations=20,
@@ -13,8 +16,8 @@ import os
     save_path=os.path.join(".", "models", "model-save-path.h5")
 )"""
 
-load_path = os.path.join(".", "models", "model-4.h5")
 
+# check how far the model can get (n_shuffles) with random cubes
 """max_length_solving_seq, max_solved_n_shuffles = inference(
     load_path=load_path,
     max_steps_until_solved=50
@@ -25,9 +28,32 @@ print(f"max_length_solving_seq = {max_length_solving_seq}")
 print(f"max_solved_n_shuffles = {max_solved_n_shuffles}")"""
 
 
-steps, solved = single_random_cube_solve(n_shuffles=5, model_path=load_path)
+# solve single random cube with specified n_shuffles
+"""steps, solved = single_random_cube_solve(n_shuffles=5, model_path=load_path)
 if solved:
     print("Solved! Steps:")
     print(steps)
 else:
-    print("Failed to solve cube!")
+    print("Failed to solve cube!")"""
+
+
+# solve specific cube (user defined)
+f = ["yellow", "yellow", "yellow", "green", "green", "white", "green", "green", "white"]
+r = ["red", "blue", "blue", "orange", "orange", "white", "orange", "orange", "white"]
+l = ["yellow", "green", "orange", "yellow", "red", "white", "red", "red", "white"]
+b = ["yellow", "yellow", "red", "blue", "blue", "red", "blue", "blue", "white"]
+u = ["blue", "orange", "orange", "red", "yellow", "yellow", "green", "green", "green"]
+d = ["orange", "orange", "blue", "white", "white", "blue", "green", "red", "red"]
+
+cube = make_cube_from_flattened_sides(f, r, l, b, u, d)
+print(cube)
+
+model = build_model()
+model.load_weights(load_path)
+
+steps, solved = solve_cube(cube, model)
+if solved:
+    print("Solved! Steps:")
+    print(steps)
+else:
+    print("Failed to solve!")
--- playground.py
+++ playground.py
@@ -1,1 +1,12 @@
 import pycuber as pc
+from help_functions import make_cube_from_flattened_sides
+
+
+f = ["red", "blue", "yellow", "yellow", "yellow", "red", "white", "red", "blue"]
+r = ["green", "green", "red", "yellow", "green", "red", "blue", "white", "white"]
+l = ["yellow", "red", "white", "white", "white", "blue", "yellow", "white", "blue"]
+b = ["blue", "blue", "green", "green", "blue", "red", "yellow", "yellow", "red"]
+u = ["orange", "orange", "orange", "blue", "orange", "orange", "red", "red", "white"]
+d = ["white", "blue", "blue", "yellow", "blue", "red", "orange", "red", "red"]
+
+print(make_cube_from_flattened_sides(f, r, l, b, u, d))
