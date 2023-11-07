fixed ram-overflow and improved execution speed

deepcubea_approach/inference.py
@@ -1,12 +1,14 @@
 import inference
 import data_utils as data
 import model_utils as m_utils
+import numpy as np
 
 
 model = m_utils.build_model_residual()
 model.load_weights("/Users/marcokleimaier/Documents/Projekte/RubiksCubeSolver/deepcubea_approach/saved_models/model_copy.h5")
 
 solved_n_shuffles = []
+solution_lens = []
 n_shuffles = 1
 while True:
     print(f"Testing n_shuffles: {n_shuffles}")
@@ -17,7 +19,12 @@ while True:
         break
 
     solved_n_shuffles.append(n_shuffles)
+    solution_lens.append(len(sequence))
     n_shuffles += 1
     print(f"Sequence: {sequence}")
 
-print(f"Solved n_shuffles: {solved_n_shuffles}")
+solution_lens = np.array(solution_lens)
+
+print(f"\nSolved up to n_shuffles = {n_shuffles}")
+print(f"solution lens: {solution_lens}")
+print(f"Maximum solution length: {solution_lens.max()}")

deepcubea_approach/model_utils/help_functions.py
@@ -5,14 +5,19 @@ import data_utils as data
 from .predict import predict_cost_to_go_from_cube
 
 
+def get_inverse_action(action: str) -> str:
+    return (action + "'").replace("''", "")
+
+
 def get_updated_cost_to_go_value(cube: Cube, model: Model) -> float:
-    all_child_values = []
+    min_pred_cost_to_go = 999_999
 
     for action in data.ACTIONS:
-        cube_copy = cube.copy()
-        cube_copy(action)
-        predicted_cost_to_go_successor = predict_cost_to_go_from_cube(cube_copy, model)
+        cube(action)
+        predicted_cost_to_go_successor = predict_cost_to_go_from_cube(cube, model)
+        cube(get_inverse_action(action))
 
-        all_child_values.append(predicted_cost_to_go_successor + 1)
+        if predicted_cost_to_go_successor < min_pred_cost_to_go:
+            min_pred_cost_to_go = predicted_cost_to_go_successor
 
-    return np.min(all_child_values)
+    return min_pred_cost_to_go + 1